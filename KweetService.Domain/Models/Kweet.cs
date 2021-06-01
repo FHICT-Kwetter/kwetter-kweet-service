39a1d1bd93844021a8e920ebee1db80b95bb3d79
Heijnen,Dirk D.J.C <djc.heijnen@student.fontys.nl>
2021-06-01 17:12:20 +0200

    Fixed service/domain layer

--- KweetService.Domain/Models/Kweet.cs
+++ KweetService.Domain/Models/Kweet.cs
@@ -1,36 +1,16 @@
-﻿// <copyright file="Kweet.cs" company="Kwetter">
-//     Copyright Kwetter. All rights reserved.
-// </copyright>
-// <author>Dirk Heijnen</author>
+﻿using System;
+using KweetService.Domain.Text.Models;
 
 namespace KweetService.Domain.Models
 {
-    using System;
-    using KweetService.Domain.Text.Models;
-
-    /// <summary>
-    /// The kweet model.
-    /// </summary>
     public class Kweet
     {
-        /// <summary>
-        /// The identifier of the kweet.
-        /// </summary>
         public Guid Id { get; set; }
 
-        /// <summary>
-        /// The identifier of the user who created the kweet.
-        /// </summary>
         public Guid AuthorId { get; set; }
 
-        /// <summary>
-        /// The date when the kweet was created.
-        /// </summary>
         public DateTime CreatedAt { get; set; }
 
-        /// <summary>
-        /// The text of the kweet.
-        /// </summary>
         public KweetText Text { get; set; }
     }
 }
